a95f47783e0fd0709516c75f219dfc59ba519842
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-07-14 18:30:42 -0300

    repository, get and post new transaction done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,27 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        if (transaction.type === 'income') {
+          return {
+            income: accumulator.income + Number(transaction.value),
+            outcome: accumulator.outcome,
+          };
+        }
+        return {
+          income: accumulator.income,
+          outcome: accumulator.outcome + Number(transaction.value),
+        };
+      },
+      { income: 0, outcome: 0 },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
